1b7d4152f800123e8e87e7101649f3818c61d842
Antonio Rodriguez <antonio@yeboyebo.es>
2025-04-05 19:59:58 +0200

    refact(cliente): Tipado general para sacar funciones comunes de cliente

--- src/contextos/ventas/cliente/dominio.ts
+++ src/contextos/ventas/cliente/dominio.ts
@@ -1,21 +1,22 @@
+import { Entidad } from "../../comun/diseño.ts";
 import { stringNoVacio } from "../../comun/dominio.ts";
 import { Cliente, DirCliente, NuevaDireccion, NuevoCliente } from "./diseño.ts";
 
 export const idFiscalValido = (tipo: string) => (valor: string) => {
     if (tipo === "NIF") {
-        return valor.length === 9;
+        return valor.length === 9 || "El NIF debe tener 9 caracteres";
     }
     if (tipo === "NAF") {
-        return valor.length === 11 && valor[0] === "E" && valor[1] === "S";
+        return (valor.length === 11 && valor[0] === "E" && valor[1] === "S") || "El NAF debe cumplir ESXXXXXXXXXX";
     }
     return false;
 }
-export const tipoIdFiscalValido = (tipo: string) => {
-    return tipo === "NIF" || tipo === "NAF";
+export const tipoIdFiscalValido = (tipo: string): string | boolean => {
+    return tipo === "NIF" || tipo === "NAF" || "El tipo debe ser NIF o NAF";
 }
 
 export const idFiscalValidoGeneral = (tipo: string, valor: string) => {
-    return idFiscalValido(tipo)(valor) && tipoIdFiscalValido(tipo);
+    return idFiscalValido(tipo)(valor) && tipoIdFiscalValido(tipo) === true;
 }
 
 // export const guardar = async (_: string, __: Partial<Cliente>) => {
@@ -63,29 +64,25 @@ export const validadoresCliente = {
 };
 
 
-// const simularApi = async () => {
-//     const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
-//     await delay(700);
-// }
-
-export type EstadoCliente = {
-    valor: Cliente;
-    valor_inicial: Cliente;
+export type EstadoEntidad<T extends Entidad> = {
+    valor: T;
+    valor_inicial: T;
     validacion: Validacion;
 }
+export type EstadoCliente = EstadoEntidad<Cliente>;
 type ValidacionCampo = {
     valido: boolean;
     advertido: boolean;
-    // erroneo: boolean;
     textoValidacion: string;
     deshabilitado: boolean;
+    requerido: boolean;
 }
 
 export type Validacion = Record<
     string, ValidacionCampo
 >;
 
-export const puedoGuardarCliente = (estado: EstadoCliente) => {
+export const puedoGuardarCliente = (estado: EstadoEntidad<Cliente>) => {
     const valor_inicial = estado.valor_inicial;
     const valor = estado.valor;
     return (
@@ -94,46 +91,76 @@ export const puedoGuardarCliente = (estado: EstadoCliente) => {
     )
 }
 
-export const initEstadoCliente = (cliente: Cliente): EstadoCliente => {
-    const deshabilitados = ['nombre_agente'];
+const initEstadoEntidad = <T extends Entidad>(entidad: T, deshabilitados: string[], requeridos: string[]) => {
     const validacion: Validacion = {}
-    for (const k in cliente) {
+    for (const k in entidad) {
         validacion[k] = {
             valido: true,
             advertido: false,
             textoValidacion: "",
             deshabilitado: deshabilitados.includes(k),
+            requerido: requeridos.includes(k),
         };
     }
     const estado = {
-        valor: cliente,
-        valor_inicial: { ...cliente },
+        valor: entidad,
+        valor_inicial: { ...entidad },
         validacion
     }
     return estado;
 }
 
+export const initEstadoCliente = (cliente: Cliente): EstadoCliente => {
+    const deshabilitados = ['nombre_agente'];
+    const requeridos = [
+        'nombre',
+        'tipo_id_fiscal',
+        'id_fiscal'
+    ];
+    return initEstadoEntidad(cliente, deshabilitados, requeridos);
+}
+
 export const cambiarCliente = (cliente: EstadoCliente, campo: string, valor: string): EstadoCliente => {
     return validarCambio(
         cambiarCampo(cliente, campo, valor),
-        campo
+        campo, validar
     );
-
 }
 
-const cambiarCampo = (cliente: EstadoCliente, campo: string, valor: string): EstadoCliente => {
+
+
+// const cambiarCampo = (cliente: EstadoCliente, campo: string, valor: string): EstadoCliente => {
+//     return {
+//         ...cliente,
+//         valor: {
+//             ...cliente.valor,
+//             [campo]: valor
+//         }
+//     }
+// }
+const cambiarCampo = <T extends Entidad>(estado: EstadoEntidad<T>, campo: string, valor: string): EstadoEntidad<T> => {
     return {
-        ...cliente,
+        ...estado,
         valor: {
-            ...cliente.valor,
+            ...estado.valor,
             [campo]: valor
         }
     }
 }
-const validarCambio = (cliente: EstadoCliente, campo: string): EstadoCliente => {
+
+// const validarCambio = (cliente: EstadoCliente, campo: string): EstadoCliente => {
+//     return {
+//         ...cliente,
+//         validacion: validar(cliente, campo)
+//     }
+// }
+const validarCambio = <T extends Entidad>(estado: EstadoEntidad<T>,
+    campo: string,
+    validador: Validador<T>
+): EstadoEntidad<T> => {
     return {
-        ...cliente,
-        validacion: validar(cliente, campo)
+        ...estado,
+        validacion: validador(estado, campo)
     }
 }
 
@@ -191,50 +218,42 @@ export const estadoAInput = (estado: EstadoCliente, campo: string): EstadoClient
 }
 
 const validaciones = {
-    nombre: (validacion: ValidacionCampo, valor: string): ValidacionCampo => {
-        return {
-            ...validacion,
-            ...validarRequerido(validacion, valor),
-        }
-    },
     tipo_id_fiscal: (validacion: ValidacionCampo, valor: string): ValidacionCampo => {
         const valido = tipoIdFiscalValido(valor);
         return {
             ...validacion,
-            valido: valido,
-            advertido: false,
-            textoValidacion: valido ? "" : "Tipo de ID Fiscal no válido",
+            valido: valido === true,
+            textoValidacion: typeof valido === "string" ? valido : "",
         }
     },
     id_fiscal: (validacion: ValidacionCampo, valor: string, cliente: EstadoCliente): ValidacionCampo => {
         const tipoValido = tipoIdFiscalValido(cliente.valor.tipo_id_fiscal);
-        const valido = tipoValido !== true || idFiscalValido(cliente.valor.tipo_id_fiscal)(valor);
+        const valido = tipoValido
+            ? idFiscalValido(cliente.valor.tipo_id_fiscal)(valor)
+            : true;
         return {
             ...validacion,
             // ...validarRequerido(validacion, valor),
-            valido: valido,
-            textoValidacion: valido ? "" : "Formato no válido",
+            valido: valido === true,
+            textoValidacion: typeof valido === "string" ? valido : "",
         }
     },
 }
 
-const validarRequerido = (validacion: ValidacionCampo, valor: string): ValidacionCampo => {
+const validacionDefecto = (validacion: ValidacionCampo, valor: string): ValidacionCampo => {
+    const valido = !validacion.requerido || stringNoVacio(valor);
     return {
         ...validacion,
-        valido: stringNoVacio(valor),
-        textoValidacion: stringNoVacio(valor) ? "" : "Campo requerido",
+        valido,
+        textoValidacion: valido ? "" : "Campo requerido",
     }
 }
-const validar = (estado: EstadoCliente, campo: string): Validacion => {
+type Validador<T extends Entidad> = (estado: EstadoEntidad<T>, campo: string) => Validacion;
+
+const validar: Validador<Cliente> = (estado, campo) => {
     const cliente = estado.valor;
     const validacion = estado.validacion;
     switch (campo) {
-        case "nombre": {
-            return {
-                ...validacion,
-                [campo]: validaciones.nombre(validacion[campo], cliente[campo]),
-            };
-        }
         case "tipo_id_fiscal": {
             return {
                 ...validacion,
@@ -242,14 +261,22 @@ const validar = (estado: EstadoCliente, campo: string): Validacion => {
                 id_fiscal: validaciones.id_fiscal(validacion.id_fiscal, cliente.id_fiscal, estado),
             };
         }
-        case "id_fiscal": {
-            return {
-                ...validacion,
-                id_fiscal: validaciones.id_fiscal(validacion.id_fiscal, cliente.id_fiscal, estado),
-            };
+        default: {
+            if (campo in validaciones) {
+                // @ts-ignore
+                const validacionCampo = validaciones[campo];
+                return {
+                    ...validacion,
+                    [campo]: validacionCampo(validacion[campo], cliente[campo], estado),
+                }
+            } else {
+                return {
+                    ...validacion,
+                    [campo]: validacionDefecto(validacion[campo], cliente[campo] as string),
+                }
+            }
         }
     }
-    return validacion
 }
 
 export const initEstadoClienteVacio = () => initEstadoCliente(clienteVacio())
